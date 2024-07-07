e0785cb364c79594fc8135f55954f7c9f8b7bb31
erleibiazzio <erleibiazzio@gmail.com>
2024-07-07 15:13:21 -0300

    Configura o sass

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,22 @@
-const { defineConfig } = require('@vue/cli-service')
+// eslint-disable-next-line no-unused-vars
+const path = require('path');
 
-module.exports = defineConfig({
+module.exports = {
   transpileDependencies: true,
   pluginOptions: {
     electronBuilder: {
       externals: ['mariadb'],
       nodeIntegration: true,
-    }
-  }
-})
+    },
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        implementation: require('sass'),
+        sassOptions: {
+          additionalData: `@import "@/assets/sass/global.scss";`,
+        },
+      },
+    },
+  },
+};
