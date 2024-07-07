bb9cca4df40cc12fce3987c03c769fbbe76b0592
erleibiazzio <erleibiazzio@gmail.com>
2024-07-07 15:13:05 -0300

    Instala bootstrap e sass

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import './assets/sass/global.scss'
+
+const app = createApp(App);
+
+
+import EAuth from './components/e-auth/ErIndex.vue';
+
+app.component('e-auth', EAuth);
+
+app.mount('#app');
