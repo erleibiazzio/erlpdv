a412b293b1ef5b5d0bc04cb0bc6ebe96d004a4a5
erleibiazzio <erleibiazzio@gmail.com>
2024-07-08 01:12:56 -0300

    Implementa métodos para buscar dados na entidade

--- src/abstracts/Entity.js
+++ src/abstracts/Entity.js
@@ -1,24 +1,67 @@
 // src/abstracts/Entity.js
-const { Model } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 
 class Entity extends Model {
-  static init(attributes, options) {
-    options = options || {};
-    options.timestamps = true; // Adding common option for timestamps
-    return super.init(attributes, options);
-  }
+    static init(attributes, options) {
 
-  static commonMethod() {
-    console.log('This is a common method');
-  }
+        const firstAtributes = {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true
+            },
+        }
 
-  get commonAttribute() {
-    return this.getDataValue('commonAttribute');
-  }
+        const LastAttributes = {
+            createdAt: {
+                type: DataTypes.DATE,
+                allowNull: false,
+                defaultValue: DataTypes.NOW
+            },
+            updatedAt: {
+                type: DataTypes.DATE,
+                allowNull: false,
+                defaultValue: DataTypes.NOW
+            },
+        }
 
-  set commonAttribute(value) {
-    this.setDataValue('commonAttribute', value);
-  }
+        const AllAtributes = {
+            ...firstAtributes,
+            ...attributes,
+            ...LastAttributes,
+        };
+
+        options = options || {};
+        options.timestamps = true; // Adding common option for timestamps
+        return super.init(AllAtributes, options);
+    }
+
+    /**
+     * Devolve todas as entidades
+     * @returns Entity[]
+     */
+    static async find() {
+        try {
+            const entities = await this.findAll();
+            return entities;
+        } catch (error) {
+            throw new Error(`Failed to fetch entities: ${error.message}`);
+        }
+    }
+
+    /**
+     * Devolve todas as entidades com base nos filtros
+     * @param {*} filter 
+     * @returns Entity[]
+     */
+    static async findBy(filter) {
+        try {
+            const entity = await this.findOne({ where: filter });
+            return entity;
+        } catch (error) {
+            throw new Error(`Failed to fetch entity: ${error.message}`);
+        }
+    }
 }
 
 module.exports = Entity;
