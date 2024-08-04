7d2d81b8c1a442917da2c72026b6524bd713bf1f
erleibiazzio <erleibiazzio@gmail.com>
2024-08-04 14:30:28 -0300

    Ajusta vueRouter para barrar acesso a area de usuarios caso o user logado nao tenha permissão

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,12 @@ import Licence from '../models/Licence';
 import { getSession } from '../session';
 import User from '../models/User';
 
-
 // Importe seus componentes e configure suas rotas
 import EDashboard from '../components/e-dashboard/ErIndex.vue';
 import EAuth from '../components/e-auth/ErIndex.vue';
 import EUsers from '../components/e-users/ErIndex.vue';
 import Eactive from '../components/e-active/ErIndex.vue';
+import { loadModel } from '@/Helpers/loadModels';
 
 const routes = [
     {
@@ -25,13 +25,13 @@ const routes = [
         path: '/',
         name: 'e-dashboard',
         component: EDashboard,
-        meta: { requiresAuth: true } // Esta rota requer autenticação
+        meta: { requiresAuth: true }
     },
     {
         path: '/users',
         name: 'e-users',
         component: EUsers,
-        meta: { requiresAuth: true } // Esta rota requer autenticação
+        meta: { requiresAuth: true, requiresPermission: 'User:AccessArea' } // Adicione uma meta para permissão
     },
 
     // Outras rotas aqui
@@ -54,6 +54,13 @@ router.beforeEach(async (to, from, next) => {
         next('/login');
     } else if (isAuthenticated && to.path === '/login') {
         next('/');
+    } else if (isAuthenticated && to.meta.requiresPermission) {
+        const hasPermission = await checkPermission(to.meta.requiresPermission);
+        if (!hasPermission) {
+            next('/');
+        } else {
+            next();
+        }
     } else {
         next();
     }
@@ -63,26 +70,40 @@ async function checkAuthentication() {
     let sessionStorage = localStorage.getItem('sessionId');
     if (sessionStorage) {
         let session = await getSession(sessionStorage);
-        if(session) {
+        if (session) {
             let user = await User.findByPk(session.userId);
 
-            if(!user) {
+            if (!user) {
                 return false;
             }
-    
-            if(user.status != 1) {
-                session.destroy()
+
+            if (user.status !== 1) {
+                await session.destroy();
                 return false;
             }
-    
-            if (session.sessionId == sessionStorage) {
+
+            if (session.sessionId === sessionStorage) {
                 return true;
             }
         }
-       
     }
 
     return false;
+}
+
+async function checkPermission(permission) {
+    let sessionStorage = localStorage.getItem('sessionId');
+    const _permission = permission.split(":");
+    const model = await loadModel(_permission[0])
+    const entity = new model();
+
+    if (sessionStorage) {
+        let session = await getSession(sessionStorage);
+        if (session) {
+            let user = await User.findByPk(session.userId);
+            return await entity.canUser('accessArea', user.id)
+        }
+    }
 }
 
 export default router;
